bc4c8cbe247159deeda60905648ec93062ff7ebf
Ravi Sinha <ravisinha@my.unt.edu>
2026-03-11 18:02:20 -0500

    Checkpoint: running dummy services and Kafka

--- leaderboard-service/src/main/java/com/ravisomsinha/LeaderBoardServiceApplication.java
+++ leaderboard-service/src/main/java/com/ravisomsinha/LeaderBoardServiceApplication.java
@@ -1,7 +1,11 @@
 package com.ravisomsinha;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class LeaderBoardServiceApplication {
     public static void main(String[] args) {
-
+        SpringApplication.run(LeaderBoardServiceApplication.class, args);
     }
 }
