04107daddb0eb43fc55e4d59426a9cf8984a52ea
Ravi Sinha <ravisinha@my.unt.edu>
2026-03-17 18:50:15 -0500

    Add Kafka event and serde

---
+++ score-service/src/main/java/com/ravisomsinha/model/ScoreEvent.java
@@ -0,0 +1,13 @@
+package com.ravisomsinha.model;
+
+/**
+ * Using playerId as key means all events from the same player
+ * end up in the same Kafta topic
+ *
+ * @param playerId
+ * @param game
+ * @param score
+ * @param timestamp
+ */
+public record ScoreEvent(String playerId, String game, int score, long timestamp) {}
+
